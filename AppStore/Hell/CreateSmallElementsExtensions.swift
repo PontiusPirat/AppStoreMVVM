ab7b135e7402b397ecc6cdc5ec04ab88e1bd4511
Pontius <pontius.pirat@gmail.com>
2021-08-25 14:49:52 +0300

    small ref

---
+++ AppStore/Hell/CreateSmallElementsExtensions.swift
@@ -0,0 +1,65 @@
+//
+//  CreateSmallElementsExtensions.swift
+//  AppStore
+//
+//  Created by Pontius Pirat on 25.08.2021.
+//
+
+import UIKit
+
+// MARK: - Small elements for BaseTabBarController
+
+extension BaseTabBarController {
+    
+    func createNavController(viewController: UIViewController, imageName: String, color: UIColor, title: String) -> UINavigationController {
+        
+        let navViewController = UINavigationController(rootViewController: viewController)
+        viewController.view.backgroundColor = color
+        viewController.title = title
+        navViewController.tabBarItem.image = UIImage(systemName: imageName)
+        navViewController.navigationBar.prefersLargeTitles = true
+        //navViewController.navigationBar.backgroundColor = .blue
+        
+        return navViewController
+        
+    }
+}
+
+// MARK: - Small elements for SearchResultCell
+
+extension SearchResultCell {
+    
+    // Icon image
+    func createAppIconImage(appIconImage: UIImageView, backgroundcolor: UIColor, widthAnchor: CGFloat,
+                            heightAnchor: CGFloat, cornerRadius: CGFloat) -> UIImageView {
+        
+        appIconImage.backgroundColor = backgroundcolor
+        appIconImage.widthAnchor.constraint(equalToConstant: widthAnchor).isActive = true
+        appIconImage.heightAnchor.constraint(equalToConstant: heightAnchor).isActive = true
+        appIconImage.layer.cornerRadius = cornerRadius
+        return appIconImage
+        
+    }
+    
+    // Label
+    func createAnyLabel(label: UILabel, text: String) -> UILabel {
+        label.text = text
+        return label
+    }
+    
+    // Button
+    func createButton(button: UIButton, title: String, titleColor: UIColor, fontSize: CGFloat, backgroundcolor: UIColor,
+                      cornerRadius: CGFloat, widthAnchor: CGFloat, heightAnchor: CGFloat) -> UIButton {
+        
+        button.setTitle(title, for: .normal)
+        button.setTitleColor(titleColor, for: .normal)
+        button.titleLabel?.font = .boldSystemFont(ofSize: fontSize)
+        button.backgroundColor = backgroundcolor //(5)
+        button.layer.cornerRadius = cornerRadius
+        button.widthAnchor.constraint(equalToConstant: widthAnchor).isActive = true
+        button.heightAnchor.constraint(equalToConstant: heightAnchor).isActive = true
+        return button
+        
+    }
+}
+
--- AppStore/View/CllectionViewController/Elements/SearchResultCell.swift
+++ AppStore/View/CllectionViewController/Elements/SearchResultCell.swift
@@ -12,25 +12,28 @@ class SearchResultCell: UICollectionViewCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = .systemGray6
+        let appNameLabel: UILabel = self.createAnyLabel(label: UILabel(), text: "APP NAME")
+        let categoryLabel: UILabel = self.createAnyLabel(label: UILabel(), text: "Photo & Video")
+        let ratingLabel: UILabel = self.createAnyLabel(label: UILabel(), text: "Rating")
         
-        let labelStackView = UIStackView(arrangedSubviews: [createAnyLabel(label: UILabel(), text: "APP NAME"),
-                                                            createAnyLabel(label: UILabel(), text: "Photo & Video"),
-                                                            createAnyLabel(label: UILabel(), text: "Rating")
-                                                           ])
+        let labelStackView = UIStackView(arrangedSubviews: [appNameLabel, categoryLabel, ratingLabel])
         labelStackView.axis = .vertical
         
-        let stackView = UIStackView(arrangedSubviews: [createAppIconImage(appIconImage: UIImageView(),backgroundcolor: .red,
-                                                                          widthAnchor: 64, heightAnchor: 64, cornerRadius: 12),
-                                                       labelStackView,
-                                                       createButton(button: UIButton(), title: "GET", titleColor: .blue,
-                                                                    fontSize: 14, backgroundcolor: .systemGray3, cornerRadius: 16,
-                                                                    widthAnchor: 80, heightAnchor: 32)])
+        
+        let iconImage = self.createAppIconImage(appIconImage: UIImageView(),backgroundcolor: .red,
+                                                widthAnchor: 64, heightAnchor: 64, cornerRadius: 12)
+        
+        let getButton = self.createButton(button: UIButton(), title: "GET", titleColor: .blue,
+                                          fontSize: 14, backgroundcolor: .systemGray3, cornerRadius: 16,
+                                          widthAnchor: 80, heightAnchor: 32)
+        
+        let stackView = UIStackView(arrangedSubviews: [iconImage, labelStackView, getButton])
+        
         addSubview(stackView)
         stackView.alignment = .center
         stackView.spacing = 12
-        
         stackView.fillSuperview(padding: .init(top: 16, left: 16, bottom: 16, right: 16))
+        backgroundColor = .systemGray6
     }
     
     required init?(coder: NSCoder) {
